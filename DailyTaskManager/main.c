c1ef5c77efa8d51b2c739aca09874c20198ffe84
Fergus Cooney <ferguscasala@gmail.com>
2013-11-13 13:28:26 +0000

    Initial Commit

---
+++ DailyTaskManager/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  DailyTaskManager
+//
+//  Created by Fergus Cooney on 13/11/2013.
+//  Copyright (c) 2013 Fergus Cooney. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
